7fc10f4646e3b6010e3759b9ca75d460d8671efc
JosephMumo <josephmumo25@gmail.com>
2023-05-04 17:02:22 +0300

    React state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0,
+      message: ''
+    }
+  }
+
+  upCount() {
+    this.setState({
+      count : this.state.count + 1
+    })
+  }
+  downCount() {
+    this.setState({
+      count :  this.state.count - 1
+    })
+  }
+  changeMessage() {
+    this.setState({
+      message: `${this.state.count}  saved succesfully!!`
+    })
+  }
+  
+  render() {
+    return(
+      <div className='App'>
+        <h2>Counter</h2>
+        <h1>{this.state.count}</h1>
+        <hr />
+        <div className='section'>
+          <button onClick={() => this.upCount()}>+</button>
+          <button onClick={() => this.downCount()}>-</button>
+        </div>
+        <button className='btn' onClick={() => this.changeMessage()}>Save Count</button>
+        <p>{this.state.message}</p>
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
